aabec6bd868ec26f45f5b712232f28b83c4666fa
drazzull <arthur.bilibio92@gmail.com>
2016-03-29 22:01:31 -0300

    Commit inicial

---
+++ SupervisorioIA2T1/SupervisorioIA2T1/VerticalProgressBar.cs
@@ -0,0 +1,61 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+/// <summary>
+/// Objeto personalizado para fazer uma ProgressBar vertical
+/// </summary>
+public class VerticalProgressBar : ProgressBar
+{
+    /// <summary>
+    /// Construtor da classe
+    /// </summary>
+    public VerticalProgressBar()
+    {
+        this.SetStyle(ControlStyles.UserPaint, true);
+    }
+
+    /// <summary>
+    /// Sobrescrita do método CreateParams para fazer a barra vertical
+    /// </summary>
+    protected override CreateParams CreateParams
+    {
+        get
+        {
+            CreateParams cp = base.CreateParams;
+            cp.Style |= 0x04;
+            return cp;
+        }
+    }
+
+    /// <summary>
+    /// Sobrescrita do método OnPaint para mudar a cor da barra
+    /// </summary>
+    /// <param name="e">Evento de pintura</param>
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        Rectangle rec = e.ClipRectangle;
+        rec.Width = rec.Width - 4;
+        int heightTemp = (int)(rec.Height * ((double)this.Value / this.Maximum)) - 4;
+        heightTemp = rec.Height - heightTemp;
+        double percentual = (this.Value * 100) / this.Maximum;
+
+        if (ProgressBarRenderer.IsSupported)
+        {
+            ProgressBarRenderer.DrawVerticalBar(e.Graphics, e.ClipRectangle);
+        }
+
+        if (percentual <= 50)
+        {
+            e.Graphics.FillRectangle(Brushes.Blue, 2, heightTemp, rec.Width, rec.Height);
+            return;
+        }
+
+        if (percentual <= 75)
+        {
+            e.Graphics.FillRectangle(Brushes.Yellow, 2, heightTemp, rec.Width, rec.Height);
+            return;
+        }
+
+        e.Graphics.FillRectangle(Brushes.Red, 2, heightTemp, rec.Width, rec.Height);
+    }
+}
