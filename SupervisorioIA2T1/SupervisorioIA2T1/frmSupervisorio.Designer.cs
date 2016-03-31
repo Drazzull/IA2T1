3c2a2620ab8bbb61337a64676de59407110ab82a
drazzull <arthur.bilibio92@gmail.com>
2016-03-31 13:51:55 -0300

    Ajuste da Tela Principal - Prévia

--- SupervisorioIA2T1/SupervisorioIA2T1/frmSupervisorio.Designer.cs
+++ SupervisorioIA2T1/SupervisorioIA2T1/frmSupervisorio.Designer.cs
@@ -28,20 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnTreino = new System.Windows.Forms.Button();
+            this.btnProducao = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::SupervisorioIA2T1.Properties.Resources.Cooler;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.InitialImage = global::SupervisorioIA2T1.Properties.Resources.Cooler;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(229, 218);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnTreino
+            // 
+            this.btnTreino.Location = new System.Drawing.Point(435, 236);
+            this.btnTreino.Name = "btnTreino";
+            this.btnTreino.Size = new System.Drawing.Size(147, 23);
+            this.btnTreino.TabIndex = 1;
+            this.btnTreino.Text = "Iniciar Treinamento";
+            this.btnTreino.UseVisualStyleBackColor = true;
+            // 
+            // btnProducao
+            // 
+            this.btnProducao.Location = new System.Drawing.Point(588, 236);
+            this.btnProducao.Name = "btnProducao";
+            this.btnProducao.Size = new System.Drawing.Size(122, 23);
+            this.btnProducao.TabIndex = 2;
+            this.btnProducao.Text = "Iniciar Produção";
+            this.btnProducao.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::SupervisorioIA2T1.Properties.Resources.Cooler;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.InitialImage = global::SupervisorioIA2T1.Properties.Resources.Cooler;
+            this.pictureBox2.Location = new System.Drawing.Point(247, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(229, 218);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::SupervisorioIA2T1.Properties.Resources.Cooler;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox3.InitialImage = global::SupervisorioIA2T1.Properties.Resources.Cooler;
+            this.pictureBox3.Location = new System.Drawing.Point(482, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(229, 218);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // frmSupervisorio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(714, 318);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(722, 270);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnProducao);
+            this.Controls.Add(this.btnTreino);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmSupervisorio";
+            this.Text = "Supervisório - IA2 - T1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnTreino;
+        private System.Windows.Forms.Button btnProducao;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
 
